a0d00539d1e72de2649f5ed01313ab03635dbf2f
Christophe-Le-Laz <christophe.le-laz@oclock.school>
2024-02-07 17:59:04 +0100

    wip api login

--- src/Controller/Api/LoginController.php
+++ src/Controller/Api/LoginController.php
@@ -3,27 +3,41 @@
 namespace App\Controller\Api;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
+use App\Entity\User;
+use Symfony\Component\Security\Http\Attribute\CurrentUser;
 
 class LoginController extends AbstractController
 {
-    #[Route('/api/login', name: 'api_login', methods:['POST'])]
-    public function apiLogin(Request $request, AuthenticationUtils $authenticationUtils): JsonResponse
-    {
-    
-        $user = $this->getUser();
+    #[Route('/api/login_check', name: 'api_login_check', methods:['POST'])]
+//     public function apiLogin(Request $request, UserInterface $user = null): JsonResponse
+//     {
+//         // Handle authentication failure
+//         if (!$user) {
+//             return $this->json("Erreur : Utilisateur inexistant", 404);
+//         }
 
-        // Handle authentication failure
-        if (!$user) {
-        return $this->json(
-                "Error : Utilisateur inexistant", 404
-            );
+//         return $this->json($user, 200, [], ['groups' => ['get_user']]);
+//     }
+// }
+
+
+     public function apiLogin(#[CurrentUser] ?User $user): Response
+      {
+     if (null === $user) {
+            return $this->json([
+                'message' => 'missing credentials',
+            ], Response::HTTP_UNAUTHORIZED);
         }
-        return $this->json($user, 200, [], ['groups' => ['get_user']]
-    );
-    }
 
-}
+        $token = $jwtManager->create($user);
+
+          return $this->json([
+            'user'  => $user->getUserIdentifier(),
+            'token' => $token,
+          ]);
+      }
+  }
 
 ?>
