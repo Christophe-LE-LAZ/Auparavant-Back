946cceee116c5ae158fab96adad428f9262eea8a
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-08 12:16:44 +0100

    moving files and sub-folders into referencing folders

--- src/Controller/PlaceController.php
+++ src/Controller/PlaceController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/place')]
+#[Route('/back/place')]
 class PlaceController extends AbstractController
 {
     /**
