c8e6de01a50922ed44083a1580bf1cdff7ea31c6
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-09 15:58:21 +0100

    dabase data updated according to updated memory

--- docs/yavaitquoiavant.sql
+++ docs/yavaitquoiavant.sql
@@ -44,6 +44,7 @@ CREATE TABLE `memory` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `location_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `place_id` int(11) NOT NULL,
   `title` varchar(30) NOT NULL,
   `content` longtext NOT NULL,
   `picture_date` datetime NOT NULL,
@@ -58,13 +59,13 @@ CREATE TABLE `memory` (
   CONSTRAINT `FK_EA6D3435A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-INSERT INTO `memory` (`id`, `location_id`, `user_id`, `title`, `content`, `picture_date`, `main_picture`, `published_at`, `created_at`, `updated_at`) VALUES
-(1,	1,	1,	'Le Panthéon en 1792',	'Le Panthéon en 1792, avec La Renommée en son sommet.n',	'1792-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pierre-Antoine_de_Machy_-_Le_Panth%C3%A9on.jpg/1280px-Pierre-Antoine_de_Machy_-_Le_Panth%C3%A9on.jpg',	NULL,	'2024-02-07 13:52:16',	NULL),
-(2,	1,	1,	'Le Panthéon de nos jours',	'Le Panthéon vu de la tour Montparnasse en 2016.',	'2016-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Panth%C3%A9on_vu_de_la_tour_Montparnasse_en_2016.jpg/1280px-Panth%C3%A9on_vu_de_la_tour_Montparnasse_en_2016.jpg',	NULL,	'2024-02-07 13:52:58',	NULL),
-(3,	2,	1,	'La naissance de la tour Eiffel',	'C’est à l’occasion de l’Exposition Universelle de 1889, date qui marquait le centenaire de la Révolution française qu\'il a été décidé de construire une tour de 300m.\r\n\r\nLes premiers coups de pelle sont donnés le 26 janvier 1887.\r\nLe 31 mars 1889, la Tour achevée en un temps record -2 ans, 2 mois et 5 jours- s’impose comme une véritable prouesse technique.',	'1887-01-26 00:00:00',	'https://www.toureiffel.paris/sites/default/files/styles/mobile_x1_560/public/2017-10/070601-014_1.JPEG?itok=3lGoqCZK',	NULL,	'2024-02-07 14:50:11',	NULL),
-(4,	2,	1,	'La Tour Eiffel de nos jours',	'La tour Eiffel [tuʁɛfɛl] est une tour de fer puddlé de 330 m de hauteur située à Paris, à l’extrémité nord-ouest du parc du Champ-de-Mars en bordure de la Seine dans le 7ᵉ arrondissement. Son adresse officielle est 5, avenue Anatole-France.',	'2009-06-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/260px-Tour_Eiffel_Wikimedia_Commons.jpg',	NULL,	'2024-02-07 14:51:04',	NULL),
-(5,	3,	1,	'Notre-Dame',	'La cathédrale Notre-Dame de Paris, communément appelée Notre-Dame, est l\'un des monuments les plus emblématiques de Paris et de la France. Elle est située sur l\'île de la Cité et est un lieu de culte catholique, siège de l\'archidiocèse de Paris, dédié à la Vierge Marie.',	'2009-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Notre-Dame_de_Paris_2009-04-28.jpg/1024px-Notre-Dame_de_Paris_2009-04-28.jpg',	NULL,	'2024-02-08 09:48:14',	NULL),
-(6,	3,	1,	'Incendie de Notre-Dame',	'L’incendie de Notre-Dame de Paris est un incendie majeur survenu à la cathédrale Notre-Dame de Paris, les 15 et 16 avril 2019, pendant près de 15 heures.',	'2019-04-15 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Incendie_Notre_Dame_de_Paris.jpg/280px-Incendie_Notre_Dame_de_Paris.jpg',	NULL,	'2024-02-08 09:50:03',	NULL);
+INSERT INTO `memory` (`id`, `location_id`, `user_id`, `place_id`, `title`, `content`, `picture_date`, `main_picture`, `published_at`, `created_at`, `updated_at`) VALUES
+(1,	1,	1,	1,	'Le Panthéon en 1792',	'Le Panthéon en 1792, avec La Renommée en son sommet.n',	'1792-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Pierre-Antoine_de_Machy_-_Le_Panth%C3%A9on.jpg/1280px-Pierre-Antoine_de_Machy_-_Le_Panth%C3%A9on.jpg',	NULL,	'2024-02-07 13:52:16',	NULL),
+(2,	1,	1,	1,	'Le Panthéon de nos jours',	'Le Panthéon vu de la tour Montparnasse en 2016.',	'2016-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/b/bb/Panth%C3%A9on_vu_de_la_tour_Montparnasse_en_2016.jpg/1280px-Panth%C3%A9on_vu_de_la_tour_Montparnasse_en_2016.jpg',	NULL,	'2024-02-07 13:52:58',	NULL),
+(3,	2,	1,	2,	'La naissance de la tour Eiffel',	'C’est à l’occasion de l’Exposition Universelle de 1889, date qui marquait le centenaire de la Révolution française qu\'il a été décidé de construire une tour de 300m.\r\n\r\nLes premiers coups de pelle sont donnés le 26 janvier 1887.\r\nLe 31 mars 1889, la Tour achevée en un temps record -2 ans, 2 mois et 5 jours- s’impose comme une véritable prouesse technique.',	'1887-01-26 00:00:00',	'https://www.toureiffel.paris/sites/default/files/styles/mobile_x1_560/public/2017-10/070601-014_1.JPEG?itok=3lGoqCZK',	NULL,	'2024-02-07 14:50:11',	NULL),
+(4,	2,	1,	2,	'La Tour Eiffel de nos jours',	'La tour Eiffel [tuʁɛfɛl] est une tour de fer puddlé de 330 m de hauteur située à Paris, à l’extrémité nord-ouest du parc du Champ-de-Mars en bordure de la Seine dans le 7ᵉ arrondissement. Son adresse officielle est 5, avenue Anatole-France.',	'2009-06-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Tour_Eiffel_Wikimedia_Commons.jpg/260px-Tour_Eiffel_Wikimedia_Commons.jpg',	NULL,	'2024-02-07 14:51:04',	NULL),
+(5,	3,	1,	3,	'Notre-Dame',	'La cathédrale Notre-Dame de Paris, communément appelée Notre-Dame, est l\'un des monuments les plus emblématiques de Paris et de la France. Elle est située sur l\'île de la Cité et est un lieu de culte catholique, siège de l\'archidiocèse de Paris, dédié à la Vierge Marie.',	'2009-01-01 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3d/Notre-Dame_de_Paris_2009-04-28.jpg/1024px-Notre-Dame_de_Paris_2009-04-28.jpg',	NULL,	'2024-02-08 09:48:14',	NULL),
+(6,	3,	1,	3,	'Incendie de Notre-Dame',	'L’incendie de Notre-Dame de Paris est un incendie majeur survenu à la cathédrale Notre-Dame de Paris, les 15 et 16 avril 2019, pendant près de 15 heures.',	'2019-04-15 00:00:00',	'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Incendie_Notre_Dame_de_Paris.jpg/280px-Incendie_Notre_Dame_de_Paris.jpg',	NULL,	'2024-02-08 09:50:03',	NULL);
 
 DROP TABLE IF EXISTS `picture`;
 CREATE TABLE `picture` (
@@ -122,4 +123,4 @@ INSERT INTO `user` (`id`, `firstname`, `lastname`, `email`, `password`, `roles`,
 (1,	'Aurélien',	'ROUCHETTE-MARET',	'aurelien.rouchette@orange.fr',	'aurélien',	'[\"ROLE_ADMIN\"]',	'2024-02-07 13:44:52',	NULL),
 (3,	'Christophe',	'LE LAZ',	'christophe.le-laz@oclock.school',	'christophe',	'[\"ROLE_ADMIN\"]',	'2024-02-08 14:19:24',	NULL);
 
--- 2024-02-08 14:20:11
+-- 2024-02-09 14:57:50
