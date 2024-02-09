82220349dab01861fbdf8fce6cfe36408ad74f26
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-09 15:14:41 +0100

    Updated file

--- src/Controller/Api/MemoryController.php
+++ src/Controller/Api/MemoryController.php
@@ -55,7 +55,7 @@ class MemoryController extends AbstractController
     {
         if (!$memory) {
             return $this->json(
-                "Error : Souvenir inexistant", 404
+                "Erreur : Souvenir inexistant", 404
             );
         }
 
