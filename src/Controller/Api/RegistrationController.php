b44eabd265eaf8eae7bde808a23d145861478e09
Christophe-Le-Laz <christophe.le-laz@oclock.school>
2024-02-10 14:02:38 +0100

    update security.yaml and create API route for user registration

---
+++ src/Controller/Api/RegistrationController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Api;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class RegistrationController extends AbstractController
+{
+     /**
+     * Create a new user
+     * @param UserPasswordHasherInterface $userPasswordHasher
+     * @param EntityManagerInterface $entityManager
+     * @param Request $request
+     * @return Response
+     */
+    #[Route('/api/register', name: 'api_register', methods: ['POST'])]
+    public function registerApi(UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, Request $request): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+
+        if ($existingUser) {
+            return $this->json(['error' => 'l\'email est déjà utilisée par un autre utilisateur'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = new User();
+        $user->setFirstname($data['firstname']);
+        $user->setLastname($data['lastname']);
+        $user->setEmail($data['email']);
+        $user->setPassword(
+            $userPasswordHasher->hashPassword(
+                $user,
+                $data['password']
+            )
+        );
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+        return $this->json(['user' => $user, 'message' => 'Utilisateur enregistré'], Response::HTTP_CREATED, [], ['groups' => ['get_user']]);
+    }
+}
