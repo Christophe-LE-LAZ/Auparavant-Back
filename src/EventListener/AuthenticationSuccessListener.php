3f09d75081e066f5c8cec9cf17e5261dbca383f9
Christophe-Le-Laz <christophe.le-laz@oclock.school>
2024-02-12 17:45:49 +0100

    customization of response sent when requesting api/login_check route to include id, first name, last name

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\EventListener;
+
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+// use App\Entity\User;
+
+
+class AuthenticationSuccessListener
+{
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+
+        $data['data'] = array(
+            'id' => $user->getId(),
+            'username' => $user->getFirstname(),
+            'lastname' => $user->getLastname(),
+        );
+
+        $event->setData($data);
+    }
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccessResponse',
+        ];
+    }
+}
