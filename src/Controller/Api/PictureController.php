47eeaf6d1c89703402c6ee5932017fd316ae14fe
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-16 17:21:27 +0100

    Have a good weekend! :D

--- src/Controller/Api/PictureController.php
+++ src/Controller/Api/PictureController.php
@@ -14,7 +14,6 @@ use Nelmio\ApiDocBundle\Annotation\Model;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
