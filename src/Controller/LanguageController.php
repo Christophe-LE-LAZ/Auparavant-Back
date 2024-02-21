68ba47c27f0e62aae4c684cf0d6b9bc23dedede8
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-21 15:59:35 +0100

    One problem out of two solved

--- src/Controller/LanguageController.php
+++ src/Controller/LanguageController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Translation\LocaleSwitcher;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class LanguageController extends AbstractController
 {
-    public function switchLanguage(Request $request, string $locale): Response
-    {
-        $request->getSession()->set('_locale', $locale);
 
+
+    public function switchLanguage(LocaleSwitcher $localeSwitcher,Request $request, string $locale): Response
+    {
+        $request->getSession()->set('locale', $locale);
+        //$locale = $request->getLocale();
+        $localeSwitcher->setLocale($locale);
+       
         // Redirect back to the referring page
         return $this->redirect($request->headers->get('referer'));
     }
+
+    
+    
+
+    
 }
+
+        
