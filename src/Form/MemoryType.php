5fa96a3a767eb1533fcfa2f2c13c3694e7c3a340
AurelienROUCHETTE <aurelien.rouchette@orange.fr>
2024-02-22 10:54:09 +0100

    corrected a few things

--- src/Form/MemoryType.php
+++ src/Form/MemoryType.php
@@ -10,13 +10,21 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MemoryType extends AbstractType
 {
+    private $translator;
+
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -41,7 +49,6 @@ class MemoryType extends AbstractType
                     'label' => 'memory.form.main_picture',
                     'mapped' => false,
                     'required' => true,
-
                 ]
             )
             ->add(
