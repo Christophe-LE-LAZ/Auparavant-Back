a56d33acc32a5622288f9eb3b44fa0826dc49c69
Christophe-Le-Laz <christophe.le-laz@oclock.school>
2024-02-23 10:01:49 +0100

    create service memoryProcessor

--- src/Controller/Api/MemoryController.php
+++ src/Controller/Api/MemoryController.php
@@ -9,6 +9,7 @@ use DateTimeImmutable;
 use App\Entity\Location;
 use App\Service\FileUploader;
 use OpenApi\Attributes as OA;
+use App\Service\MemoryProcessor;
 use App\Repository\UserRepository;
 use App\Repository\PlaceRepository;
 use App\Repository\MemoryRepository;
@@ -39,16 +40,14 @@ class MemoryController extends AbstractController
 
     private $fileUploader;
     private $memoryRepository;
+    private $memoryProcessor;
 
-    public function __construct(FileUploader $fileUploader, MemoryRepository $memoryRepository)
+    public function __construct(FileUploader $fileUploader, MemoryRepository $memoryRepository, MemoryProcessor $memoryProcessor)
     {
         $this->fileUploader = $fileUploader;
         $this->memoryRepository = $memoryRepository;
+        $this->memoryProcessor = $memoryProcessor;
     }
-
-
-    
-    
 
 
     /**
@@ -273,11 +272,11 @@ class MemoryController extends AbstractController
     {
         $memories = $memoryRepository->findAll();
            // Associer la compare_picture selon la date
-        $processedMemories = $this->processMemories($memories);
+        $processedMemories = $this->memoryProcessor->processMemories($memories);
 
          return $this->json($processedMemories, Response::HTTP_OK);
     }
-    
+
     /**
      * Display a single memory by its id
      * TODO: Retrieve additional pictures from a memory
@@ -1028,83 +1027,6 @@ class MemoryController extends AbstractController
 
    
   
-       /**
-        * Boucle sur l'ensemble des souvenirs; récupère pour chaque localité la photo la plus récente et la plus ancienne.
-        * Compare les dates de souvenir pour savoir si le souvenir et le plus récent ou non.
-        * Si le souvenir est le plus récent alors lui associer la photo du souvenir le plus ancien de la localité;
-        * Si le souvenir n'est pas le plus récent de la localité alors lui associer la photo la plus récente de la localité;
-        */
-       private function processMemories(array $memories): array
-    {
-         $processedMemories = [];
 
-        foreach ($memories as $memory) {
-        $locationId = $memory->getLocation()->getId();
-    
-        // Trouver la photo la plus récente pour cette localité
-        $mostRecentMemory = $this->memoryRepository->findMostRecentMemoryByLocation($locationId);
-        $mostRecentMainPicture = $mostRecentMemory['main_picture'];
-
-         // Trouver la photo la plus ancienne pour cette localité
-        $oldestMemory = $this->memoryRepository->findOldestMemoryByLocation($locationId);
-        $oldestMainPicture = $oldestMemory['main_picture'];
-
-        // Convertir les dates en objet DateTime
-        $memoryDate = new \DateTime($memory->getPictureDate()->format('Y-m-d H:i:s'));
-        $mostRecentDate = new \DateTime($mostRecentMemory['picture_date']);
-       
-        // Comparer les dates pour déterminer le plus récent ou le plus ancien
-        if ($memoryDate < $mostRecentDate) {
-           $comparePicture = $mostRecentMainPicture;
-        } elseif ($memoryDate == $mostRecentDate) {
-           $comparePicture = $oldestMainPicture;
-        } else {
-           $comparePicture = null;
-        }
-
-        // Itération sur chaque photo additionnelle associée au memory et stockage dans le tableau $pictures[]
-        $pictures = [];
-        foreach ($memory->getPicture() as $picture) {
-            $pictures[] = [
-                'id' => $picture->getId(),
-                'picture' => $picture->getPicture(),
-            ];
-        }
-
-
-        $processedMemories[] = [
-           'id' => $memory->getId(),
-           'title' => $memory->getTitle(),
-           'content' => $memory->getContent(),
-           'picture_date' => $memory->getPictureDate(),
-           'main_picture' => $memory->getMainPicture(),
-           'compare_picture' => $comparePicture,
-           'location' => [
-            'id' => $memory->getLocation()->getId(),
-            'area' => $memory->getLocation()->getArea(),
-            'department' => $memory->getLocation()->getDepartment(),
-            'district' => $memory->getLocation()->getDistrict(),
-            'street' => $memory->getLocation()->getStreet(),
-            'city' => $memory->getLocation()->getCity(),
-            'zipcode' => $memory->getLocation()->getZipcode(),
-            'latitude' => $memory->getLocation()->getLatitude(),
-            'longitude' => $memory->getLocation()->getLongitude(),],
-            'picture' => $pictures,
-           'user' => [
-            'id' => $memory->getUser()->getId(),
-            'firstname' => $memory->getUser()->getFirstName(),
-            'lastname' => $memory->getUser()->getLastName(),
-            'email' => $memory->getUser()->getEmail(),
-            'roles' => $memory->getUser()->getRoles(),],
-           'place' => [
-             'id'=> $memory->getPlace()->getId(),
-             'name'=> $memory->getPlace()->getName(),
-             'type'=> $memory->getPlace()->getType(),
-           ],
-           ];
-        }
-
-   return $processedMemories;
-    }
 
 }
